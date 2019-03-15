f83f8523989a2398208f707f587fb42f18111567
Louise Iyengar <lki@louiseiyengar.com>
2019-03-15 13:26:05 -0400

    Fix button text not loading

--- js/app.js
+++ js/app.js
@@ -2,28 +2,32 @@
  * Project 4 - OOP Game App
  * app.js */
 
-var game = null;
+document.addEventListener('DOMContentLoaded', () => {
 
-document.addEventListener("click", (e) => {
-   if (e.target.id && e.target.id === "btn__reset") {
-      game = new Game();
-      game.startGame();
-   } else if (e.target.className) {
-      if (e.target.className === "key") {
-         game.handleInteraction(e.target);
-      } else if (e.target.className === "hint") {
-         game.handleHint(e.target);
-      }
-   }
-}, false);
+   var game = null;
 
-document.addEventListener("keyup", (e) => {
-   if (/^[a-zA-Z ]$/.test(e.key)) {
-      const keys = Array.from(document.getElementsByClassName("key"));
-      keys.forEach(key => {
-         if ((key.innerText === e.key) && (!key.disabled)) {
-            game.handleInteraction(key);
+   document.addEventListener("click", (e) => {
+      if (e.target.id && e.target.id === "btn__reset") {
+         game = new Game();
+         game.startGame();
+      } else if (e.target.className) {
+         if (e.target.className === "key") {
+            game.handleInteraction(e.target);
+         } else if (e.target.className === "hint") {
+            game.handleHint(e.target);
          }
-      });
-   }
-}, false);
+      }
+   }, false);
+
+   document.addEventListener("keyup", (e) => {
+      if (/^[a-zA-Z ]$/.test(e.key)) {
+         const keys = Array.from(document.getElementsByClassName("key"));
+         keys.forEach(key => {
+            if ((key.innerText === e.key) && (!key.disabled)) {
+               game.handleInteraction(key);
+            }
+         });
+      }
+   }, false);
+
+});
