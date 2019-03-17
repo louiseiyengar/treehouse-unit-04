656e4030c5a944d486ff9395a82939174825f072
Louise Iyengar <lki@louiseiyengar.com>
2019-03-17 10:39:56 -0400

    Ensure that for overlay screen, typing on keyboard causes you to remain on reset button and not tab off.

--- js/app.js
+++ js/app.js
@@ -24,12 +24,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
    //Handle typing a letter on a keyboard 
    document.addEventListener("keyup", (e) => {
-      //need to ensure we are viewing the phrase and keyboard screen
+      //need to ensure we are viewing the 'phrase and keyboard' screen
       if (document.getElementById("overlay").style.display === "none") {
          if (/^[a-zA-Z ]$/.test(e.key)) {
             const keys = Array.from(document.getElementsByClassName("key"));
             key = keys.filter(key => ((key.innerText === e.key) && (!key.disabled)));
             if (key.length === 1) { game.handleInteraction(key[0]); }
+         }
+      } else {
+         //for overlay screen, ensure that typing on keyboard causes you
+         //to remain on reset button and you can't tab off the button.
+         document.getElementById("btn__reset").focus();
+         if(e.key === "Tab") {
+            e.preventDefault();
          }
       }
    }, false);
